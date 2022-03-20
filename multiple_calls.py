bd78fdfa5faad6b72b4230ff610b8a18192afe7c
Valen <vlnsw@hotmail.com>
2022-03-20 18:37:03 +0000

    we are working on a different branch

--- multiple_calls.py
+++ multiple_calls.py
@@ -1,11 +1,8 @@
 
 def func_a():
-  print("hello world again!")
+  print("this is a first function")
 
 def func_b():
   print("this is a second function")
-
-def func_c():
-  print("this is a third function")
 
 func_a()
